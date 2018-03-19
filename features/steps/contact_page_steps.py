49b56b43a3ea81155347f26039099e2658cec6b8
kformanowicz <formanowicz.k@gmail.com>
2018-03-19 09:22:22 +0100

    Fixed regexes

--- features/steps/contact_page_steps.py
+++ features/steps/contact_page_steps.py
@@ -17,10 +17,10 @@ def step_impl(context, item_name):
 def step_impl(context, item_name):
     if item_name == "Phone":
         number = context.contact_page.driver.find_element(*ContactPageLocators.PHONE_NUMBER).text.split(": ")[1]
-        pattern = re.compile("^(\+48 )?500[- ]?600[- ]?700$")
+        pattern = re.compile("^(\+48)?[- ]?500[- ]?600[- ]?700$")
     else:
         number = context.contact_page.driver.find_element(*ContactPageLocators.FAX_NUMBER).text.split(": ")[1]
-        pattern = re.compile("^(\+48 )?22[- ]123[- ]45[- ]67$")
+        pattern = re.compile("^(\+48)?[- ]22[- ]123[- ]45[- ]67$")
 
     assert_that(bool(re.match(pattern, number)),
                 "Expected {} number to be \"+48 500 600 700\", and it is {}".format(item_name, number))
